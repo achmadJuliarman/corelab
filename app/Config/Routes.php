f18d83c9a78f6a21bc456d38020a966a584ed5cc
achmadJuliarman <acmadjuliarman@gmail.com>
2023-09-04 09:52:48 +0700

    menambahkan route index untuk masing masing controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'LoginController::index');
+$routes->get('/pegawai', 'PegawaiController::index');
+$routes->get('/user', 'UserController::index');
+$routes->get('/core', 'CoreController::index');
