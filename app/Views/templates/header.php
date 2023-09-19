591f4e461968612babf874616cf560d5b8912f18
Fikri Adam Pratama S <fikriadam3@gmail.com>
2023-09-19 11:05:43 +0700

    menambahkan gamnbar di header login

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -4,26 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/assets/img/esdm.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/login.css">
 
-    <title>Document</title>
+    <title>Database Core Storage BBSPGL</title>
 </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">Database Core Storage BBSPGL </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <img src="assets/img/logo.png" alt="esdm" class="ml-3 mt-2" style="width: 200px; height: auto;">
+    <div class="d-flex align-items-center ml-3"><p>&nbsp;&nbsp;&nbsp;</p>
+        <p class="m-0 text-white font-weight-bold" style="font-size: 1.2em;">Database Core Storage BBPSGL</p>
+    </div>
+</nav>
